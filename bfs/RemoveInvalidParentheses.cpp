f313b0e9538d972e641da7a00a4910e40a92b290
zRegle <zRegle@163.com>
2018-08-30 15:02:21 +0800

    补充题目描述and修正代码

--- bfs/RemoveInvalidParentheses.cpp
+++ bfs/RemoveInvalidParentheses.cpp
@@ -4,6 +4,15 @@
 #include <unordered_set>
 using namespace std;
 
+/*
+ * leetcode 301. Remove Invalid Parentheses
+ * 给定一串字符串, 以最少的步骤去除其中的括号, 使其合法, 列出所有去除后的结果
+ *
+ * 思路:
+ * 典型的bfs结构, 用队列来进行bfs, 一个unordered_set(hash实现)来保存已经遍历过的字符串
+ * 一个变量来保存符合要求的字符串的最长长度, 小于这个长度的都略过
+ */
+
 class Solution {
 public:
     vector<string> removeInvalidParentheses(string& s) {
@@ -43,6 +52,6 @@ public:
             if (str[i] == '(') cnt++;
             if (str[i] == ')' && cnt-- == 0) return false;
         }
-        return true;
+        return cnt == 0;
     }
 };
