5899e9c30050256729184ccc36e8cec5e458bca7
zReagle <zregle@163.com>
2020-12-03 21:09:44 +0800

    review, add deatil explanation

--- bfs/RemoveInvalidParentheses.cpp
+++ bfs/RemoveInvalidParentheses.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
+#include <string>
 #include <queue>
 #include <vector>
-#include <climits>
 #include <unordered_set>
 using namespace std;
 
@@ -11,38 +10,32 @@ using namespace std;
  *
  * 思路:
  * 典型的bfs结构, 用队列来进行bfs, 一个unordered_set(hash实现)来保存已经遍历过的字符串
- * 一个变量来保存符合要求的字符串的最长长度, 小于这个长度的都略过
+ * 一个变量表示是否得到合法的字符串, 如果是则不进行下一层的遍历
  */
 
 class Solution {
 public:
     vector<string> removeInvalidParentheses(string& s) {
         vector<string> res;
-        if (s.length() == 0) {
-            res.emplace_back("");
-            return res;
-        }
         queue<string> q;
         q.push(s);
         unordered_set<string> visit;
         visit.insert(s);
-        int max_len = INT_MIN;
+        bool found = false;
         while (!s.empty()) {
             string cur = q.front(); q.pop();
-            int len = cur.length();
             if (isValid(cur)) {
+                found = true;
                 res.push_back(cur);
-                max_len = len;
-                continue;
             }
-            if (len < max_len) continue;
-            for (int i = 0; i < len; i++) {
+            if (found) continue;
+            for (int i = 0; i < cur.length(); i++) {
                 if (cur[i] == '(' || cur[i] == ')') {
-                    string new_s = cur;
-                    new_s.erase(i, 1);
-                    if (visit.find(new_s) == visit.end()) {
-                        visit.insert(new_s);
-                        q.push(new_s);
+                    string next = cur;
+                    next.erase(i, 1);
+                    if (visit.find(next) == visit.end()) {
+                        visit.insert(next);
+                        q.push(next);
                     }
                 }
             }
@@ -54,7 +47,53 @@ public:
         int cnt = 0;
         for (char c : str) {
             if (c == '(') cnt++;
-            if (c == ')' && cnt-- == 0) return false;
+            else if (c == ')' && cnt-- == 0) return false;
+        }
+        return cnt == 0;
+    }
+};
+
+/* 不利用set来进行去重的方法
+ * 1.对于连续的括号而言, 只删除第一个
+ * 2.保证下一次删除的括号的位置一定在上次删除括号位置的右边
+ */
+class Solution2 {
+public:
+    vector<string> removeInvalidParentheses(string& s) {
+        vector<string> res;
+        queue<pair<string, int>> q;
+        q.push({s, 0});
+        bool found = false;
+        while (!s.empty()) {
+            auto cur = q.front(); q.pop();
+            string str = cur.first;
+            int last_remove = cur.second;
+            if (isValid(str)) {
+                found = true;
+                res.push_back(str);
+            }
+            if (found) continue;
+            //从上一次删除的位置开始
+            for (int i = last_remove; i < str.length(); i++) {
+                if (str[i] == '(' || str[i] == ')') {
+                    //对于连续的括号而言, 只删除第一个
+                    if (i == 0 || str[i - 1] != str[i]) {
+                        string next = str;
+                        next.erase(i, 1);
+                        //删除一个字符后, i就自动变成下一次开始遍历的位置了
+                        q.push({next, i});
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    bool isValid(string& str) {
+        int cnt = 0;
+        for (char c : str) {
+            if (c == '(') cnt++;
+            else if (c == ')' && cnt-- == 0) return false;
         }
         return cnt == 0;
     }
--- dfs/RemoveInvalidParentheses.cpp
+++ dfs/RemoveInvalidParentheses.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
+/* leetcode 301. Remove Invalid Parentheses
+ * 给定一串字符串, 以最少的步骤去除其中的括号, 使其合法, 列出所有去除后的结果
+ */
+#include <string>
 #include <vector>
 #include <stack>
 #include <algorithm>
 #include <climits>
+#include <unordered_set>
 using namespace std;
-
-/*
- * leetcode 301. Remove Invalid Parentheses
- * 给定一串字符串, 以最少的步骤去除其中的括号, 使其合法, 列出所有去除后的结果
- */
 
 /**
  * 思路一:
@@ -95,6 +94,94 @@ public:
     }
 };
 
+/* 经过剪枝的dfs, 两个剪枝思路:
+ *  1.确定有多少个左右括号是错误的, 删除该数目即可保证删除数目最少且有效剪枝
+ *  2.对于连续的括号, 删除任意一个都是的等价的
+ */
+class Solution2 {
+public:
+    vector<string> removeInvalidParentheses(string& s) {
+        int err_left = 0, err_right = 0;
+        /* 统计错误的左括号和右括号数目, 即我们要删除的括号数 */
+        for (char c : s) {
+            if (c == '(') err_left++;
+            else if (c == ')') {
+                if (err_left == 0) err_right++;
+                else err_left--;
+            }
+        }
+        string str;
+        dfs(s, str, 0, err_left, err_right, 0, 0);
+        return ans;
+    }
+
+private:
+    vector<string> ans;
+    /** 
+     * dfs求解
+     * 
+     * \param s 原字符串
+     * \param str 当前构建的字符串
+     * \param idx 在原字符串的idx, 即当前dfs深度
+     * \param err_left 当前还可以删除多少个左括号
+     * \param err_right 当前还可以删除多少个右括号
+     * \param cnt_left, cnt_right 对str当前的括号计数器, 实质上就是在递归过程中判断str是否合法
+     */
+    void dfs(string& s, string& str, int idx, 
+            int err_left, int err_right, int cnt_left, int cnt_right) {
+        /**
+         * 1.str中右括号数目大于左括号, str不合法, 直接退出 
+         * 2.当前已经不能够再删除括号了, 直接退出
+         */
+        if (cnt_right > 0 || err_left < 0 || err_right < 0) return;
+        if (idx == s.length()) {
+            if (cnt_left == 0 && cnt_right == 0) {
+                /* 两个计数器都为0, 合法字符串 */
+                ans.push_back(str);
+            }
+            return;
+        }
+        if (s[idx] == '(') {
+            /* 统计有多少个连续的括号 */
+            int k = idx;
+            while (k < s.length() && s[k] == '(') k++;
+            int cnt = k - idx;
+            /* 一次性全部加上连续的括号, 曲折地实现了:
+             * (1)多个连续的括号中, 只删除第一个括号
+             * (2)删除的位置一定在上次删除的右边
+             * 
+             * 因为在回溯之后, 我们会按顺序的删除连续的括号
+             * 如果不这样做, 那么比如这次递归我们在保留括号和删除括号中选择了保留
+             * 在下次递归中, 我们会遇到在保留括号和删除括号中选择了删除
+             * 这样就会造成"多个连续的括号中, 任意挑选一个删除", 从而造成了重复
+             */
+            str.append(cnt, '(');
+            dfs(s, str, k, err_left, err_right, cnt_left + cnt, cnt_right);
+            /* 回溯 */
+            str.erase(str.length() - cnt, cnt);
+
+            /* 删除当前括号 */
+            dfs(s, str, idx + 1, err_left - 1, err_right, cnt_left, cnt_right);
+        } else if (s[idx] == ')') {
+            int k = idx;
+            while (k < s.length() && s[k] == ')') k++;
+            int cnt = k - idx;
+            str.append(cnt, ')');
+            int cl = max(0, cnt_left - cnt);
+            int cr = max(0, cnt - cnt_left);
+            dfs(s, str, k, err_left, err_right, cl, cr);
+            str.erase(str.length() - cnt, cnt);
+            
+            dfs(s, str, idx + 1, err_left, err_right - 1, cnt_left, cnt_right);
+        } else {
+            /* 其他字符, 直接添加到str中 */
+            str += s[idx];
+            dfs(s, str, idx + 1, err_left, err_right, cnt_left, cnt_right);
+            str.pop_back();
+        }
+    }
+};
+
 /**
  * leetcode大神的0ms做法
  *
@@ -105,71 +192,86 @@ public:
  * 1.例如: "())", 我们分别去除第一二个右括号时, 都只有一个结果"()"
  *   因此, 我们约定在一系列连续的')'中, 我们只去除第一个
  *
- * 2.例如: "(())())())", '^'表示第一个多余的')'的位置, 即i
- *                ^
- *   如果我们每次都从prev_i开始遍历前i个字符
- *   那么第一次去除, 我们去除的是"(())())())", '^'表示去除')'的位置
- *                                ^
- *   第二次for循环, 我们还是去除的还是"(())())())", 造成的重复的结果
- *                                     ^
+ * 2.另外要注意一点, 假设s = "()()))", '^'表示第一个多余的')'的位置, 即i
+ *                              ^
+ *   如果我们每次都从0开始遍历前i个字符
+ *   那么第一次for循环, 我们去除"()()))",  '^'表示去除的')'的位置, 变成"(()))",
+ *                              ^                       
+ *   第一次for循环的下一次递归, 去除的是"(()))", 变成"(())", 第一次for循环的整个过程结束
+ *                                       ^
+ *   第二次for循环, 我们去除"()()))", 变成"()())"
+ *                            ^
+ *   第二次for循环的下一次递归, 还是从0开始, 去除的是"()())", 变成了"(())"
+ *                                                 ^
+ *   产生了重复的结果, 并且遗漏了一种结果"()()"
+ * 
+ *   本质上来说, 我们容易在递归的过程中按不同的次序删除两次了两个相同位置的括号
+ *   即第一种情况下第一次删除了第i个括号, 第二次删除了第j个括号
+ *   第二种情况下, 第一次删除了第j个括号, 第二次删除了第i个括号
+ *   两种情况产生的结果是一样的
+ * 
  *   所以我们需要一个变量来记录: 在前i个字符中遍历时, 上一次去除的')'的位置
+ *   此时第二次循环的下一次递归, 去除的就是"()())", 变成"()()"
+ *                                          ^ 
  */
-class Solution2 {
+class Solution3 {
 public:
     vector<string> res;
 
     vector<string> removeInvalidParentheses(string& s) {
-        if (s.length() == 0) {
-            res.emplace_back("");
-            return res;
-        }
-        auto p = new pair<char, char>('(', ')');
-        remove(s, 0, 0, p);
-        delete(p);
+        pair<char, char> p('(', ')');
+        dfs(s, 0, 0, p);
         return res;
     }
 
-    void remove(string& s, int prev_i, int prev_j, pair<char, char>* p) {
-        //第一层循环, 从上次结束的地方开始, 统计'(', ')'的数目
-        for (int stack = 0, i = prev_i; i < s.length(); ++i) {
-            if (s[i] == p->first) stack++;
-            if (s[i] == p->second) stack--;
-            if (stack < 0) {    //有多余的')'需要去除
-                //第二层循环, 将字符串前i个字符合法化
-                for (int j = prev_j; j <= i; ++j) {
-                    //尝试着在前i个字符中的每个位置中去除多余的')'
-                    //s[j-1] != p.second是确保一系列的')'中只去除第一个
-                    if (s[j] == p->second && (j == prev_j || s[j-1] != p->second)) {
-                        string afterRemoved = s;
-                        afterRemoved.erase((unsigned int)j, 1);
-                        remove(afterRemoved, i, j, p);
-                    }
-                }
-                return; //直接返回, 因为在递归中, 会把[i+1, s.length()]的字符串合法化
-            }
+    /* @params prev_i 表示当前s中, [0, i-1]是合法的
+     * @params prev_j 上一次删除close bracket的位置
+     */
+    void dfs(string& s, int prev_i, int prev_j, pair<char, char>& p) {
+        int count = 0;
+        int i = prev_i;
+        /* 确定第一个多余的close bracket */
+        while (i < s.length() && count >= 0) {
+            if (s[i] == p.first) count++;
+            else if (s[i] == p.second) count--;
+            i++;
         }
-        //关键的地方来了, 如果是'('多出来了呢?
-        //将字符串反转, 利用相同的代码, 过一遍
-        string rev = s;
-        reverse(rev.begin(), rev.end());
-        if (p->first == '(') {
-            auto pp = new pair<char, char>(')', '(');
-            remove(rev, 0, 0, pp);
-            delete(pp);
+        if (count < 0) {
+            /* close bracket过多 */
+            i--; /* 回退while循环多出来的一步 */
+            for (int j = prev_j; j <= i; j++) {
+                /* 尝试在不同的地方删除多余的close bracket */
+                /* s[j]是close bracket 并且 s[j]不是连续的close bracket
+                 * 当j == prev_j时, j-1超出了[prev_j, i]的范围, 因此需要多加一个条件
+                 */
+                if (s[j] == p.second && (j == prev_j || s[j-1] != s[j])) {
+                    string next = s;
+                    next.erase(j, 1);
+                    /* 删除了s[j]之后, next比s少一个字符, 
+                     * 因此i和j都不需要+1, 因为刚好抵消掉了
+                     */
+                    dfs(next, i, j, p);
+                }
+            }
         } else {
-            //在进入删除多余的'('的递归之前, 我们已经将原始的字符串反转过一遍
-            //当退出删除多余的'('的递归之后, 我们有反转了一遍, 得到原来的字符串
-            //所以这里直接加入结果集
-            res.push_back(rev);
+            if (p.first == '(') {
+                /* 此时可能open bracket比close bracket多, 需要复用代码递归处理这种情况, 需要先进行翻转
+                 * 首先是pair的翻转, 我们就能直接复用'确定第一个多余的close bracket'的while循环
+                 * 其次是字符串的翻转, 例如:"(()"
+                 * 如果我们只进行pair的翻转二不进行字符串的翻转, 那么在第一次进入while循环, count就会直接变-1而退出
+                 * 翻转后")((", 我们就能确定出错的'('
+                 */
+                pair<char, char> pp(')', '(');
+                reverse(s.begin(), s.end());
+                dfs(s, 0, 0, pp);
+            } else {
+                /* 已经生成了合法的字符串
+                 * 进入检查是否有多余的open bracket之前进行了一次字符串的翻转 
+                 * 此时需要再一次翻转才能得到结果
+                 */
+                reverse(s.begin(), s.end());
+                res.push_back(s);
+            }
         }
     }
 };
-
-int main() {
-    string s = "(()))()())";
-    Solution2 ss;
-    auto res = ss.removeInvalidParentheses(s);
-    for (auto& str : res)
-        cout<<str<<endl;
-    return 0;
-}
